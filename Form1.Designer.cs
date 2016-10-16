584f5c45e937d0b31c4d83092275522a531e7af1
Constantine Weismann <Constantine Weismann>
2016-10-16 22:47:00 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.field = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.create_primitive = new System.Windows.Forms.Button();
+            this.RenderTime = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_x_cord = new System.Windows.Forms.Label();
+            this.label_y_cord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // field
+            // 
+            this.field.AccumBits = ((byte)(0));
+            this.field.AutoCheckErrors = false;
+            this.field.AutoFinish = false;
+            this.field.AutoMakeCurrent = true;
+            this.field.AutoSwapBuffers = true;
+            this.field.BackColor = System.Drawing.Color.Black;
+            this.field.ColorBits = ((byte)(32));
+            this.field.DepthBits = ((byte)(16));
+            this.field.Location = new System.Drawing.Point(3, 4);
+            this.field.Name = "field";
+            this.field.Size = new System.Drawing.Size(600, 500);
+            this.field.StencilBits = ((byte)(0));
+            this.field.TabIndex = 0;
+            this.field.Load += new System.EventHandler(this.field_Load);
+            this.field.KeyDown += new System.Windows.Forms.KeyEventHandler(this.field_KeyDown);
+            this.field.MouseClick += new System.Windows.Forms.MouseEventHandler(this.myMouse);
+            // 
+            // create_primitive
+            // 
+            this.create_primitive.Location = new System.Drawing.Point(616, 12);
+            this.create_primitive.Name = "create_primitive";
+            this.create_primitive.Size = new System.Drawing.Size(104, 38);
+            this.create_primitive.TabIndex = 2;
+            this.create_primitive.Text = "Создать параллелограмм";
+            this.create_primitive.UseVisualStyleBackColor = true;
+            this.create_primitive.Click += new System.EventHandler(this.btn_create_primitive);
+            // 
+            // RenderTime
+            // 
+            this.RenderTime.Interval = 30;
+            this.RenderTime.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(634, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "x:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(634, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "y:";
+            // 
+            // label_x_cord
+            // 
+            this.label_x_cord.AutoSize = true;
+            this.label_x_cord.Location = new System.Drawing.Point(655, 53);
+            this.label_x_cord.Name = "label_x_cord";
+            this.label_x_cord.Size = new System.Drawing.Size(36, 13);
+            this.label_x_cord.TabIndex = 5;
+            this.label_x_cord.Text = "x cord";
+            // 
+            // label_y_cord
+            // 
+            this.label_y_cord.AutoSize = true;
+            this.label_y_cord.Location = new System.Drawing.Point(655, 66);
+            this.label_y_cord.Name = "label_y_cord";
+            this.label_y_cord.Size = new System.Drawing.Size(36, 13);
+            this.label_y_cord.TabIndex = 6;
+            this.label_y_cord.Text = "y cord";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 510);
+            this.Controls.Add(this.label_y_cord);
+            this.Controls.Add(this.label_x_cord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.create_primitive);
+            this.Controls.Add(this.field);
+            this.MaximumSize = new System.Drawing.Size(831, 549);
+            this.MinimumSize = new System.Drawing.Size(831, 549);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl field;
+        private System.Windows.Forms.Button create_primitive;
+        private System.Windows.Forms.Timer RenderTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_x_cord;
+        private System.Windows.Forms.Label label_y_cord;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// для работы с библиотекой OpenGL 
+using Tao.OpenGl;
+// для работы с библиотекой FreeGLUT 
+using Tao.FreeGlut;
+// для работы с элементом управления SimpleOpenGLControl 
+using Tao.Platform.Windows;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        private int count_clic = 0;
+        private Point[] temp_points = new Point[4];
+        public Form1()
+        {
+            InitializeComponent();
+            field.InitializeContexts();
+        }
+
+        private void Draw(Figure figure)
+        {
+            // очистка буфера цвета и буфера глубины 
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glClearColor(0, 0, 0, 1);
+            // очищение текущей матрицы 
+            Gl.glLoadIdentity();
+
+            // установка черного цвета 
+            Gl.glColor3f(0, 0, 0);
+
+            // помещаем состояние матрицы в стек матриц 
+            Gl.glPushMatrix();
+           
+            // помещаем состояние матрицы в стек матриц 
+            Gl.glPushMatrix();
+
+            Point[] points = figure.getPoints();
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glColor3d(255, 0, 0);
+            Gl.glVertex2f(points[0].X, points[0].Y);
+            Gl.glVertex2f(points[1].X, points[1].Y);
+            Gl.glVertex2f(points[2].X, points[2].Y);
+            Gl.glVertex2f(points[3].X, points[3].Y);
+            Gl.glEnd();
+
+            // возвращаем состояние матрицы 
+            Gl.glPopMatrix();
+
+            // возвращаем состояние матрицы 
+            Gl.glPopMatrix();
+
+            // отрисовываем геометрию 
+            Gl.glFlush();
+
+            // обновляем состояние элемента 
+            field.Invalidate();
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            // инициализация Glut 
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_SINGLE);
+            
+            // очистка окна 
+            Gl.glClearColor(0, 0, 0, 1);
+
+            // установка порта вывода в соответствии с размерами элемента anT 
+            Gl.glViewport(0, 0, field.Width, field.Height);
+
+
+            // настройка проекции 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+
+            // теперь необходимо корректно настроить 2D ортогональную проекцию 
+            Glu.gluOrtho2D(0.0,  (float)field.Width, 0.0, (float)field.Height);
+            
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+           
+            Gl.glLoadIdentity();
+
+            
+        }
+
+        public void btn_create_primitive(object sender, EventArgs e)
+        {
+            Draw(new Figure());
+
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void field_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            // Z и X отвечают за масштабирование 
+            if (e.KeyCode == Keys.Z)
+            {
+                // вызов функции, в которой мы реализуем масштабирование - передаем коэффициент масштабирования и выбранную ось в окне программы 
+                
+            }
+            if (e.KeyCode == Keys.X)
+            {
+                // вызов функции, в которой мы реализуем масштабирование - передаем коэффициент масштабирования и выбранную ось в окне программы 
+               
+            }
+
+            // W и S отвечают за перенос 
+            if (e.KeyCode == Keys.W)
+            {
+                // вызов функции, в которой мы реализуем перенос - передаем значение перемещения и выбранную ось в окне программы 
+                
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                // вызов функции, в которой мы реализуем перенос - передаем значение перемещения и выбранную ось в окне программы 
+                
+
+            } // A и D отвечают за поворот 
+            if (e.KeyCode == Keys.A)
+            {
+                // вызов функции, в которой мы реализуем поворот - передаем значение для поворота и выбранную ось 
+                
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                
+            }
+        }
+
+        private void field_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void myMouse(object sender, MouseEventArgs e)
+        {
+            if (count_clic < 3)
+            {
+                label_x_cord.Text = e.X + "";
+                label_y_cord.Text = field.Height - e.Y + "";
+                temp_points[count_clic].X = e.X;
+                temp_points[count_clic].Y = field.Height - e.Y;
+                count_clic++;
+            }
+            else
+            {
+                count_clic = 0;
+                if ((temp_points[2].X - temp_points[0].X) * (temp_points[1].Y - temp_points[0].Y) - (temp_points[2].Y - temp_points[0].Y) * (temp_points[1].X - temp_points[0].X) > 0)
+                {
+                    Point temp;
+                    temp = temp_points[2];
+                    temp_points[2] = temp_points[1];
+                    temp_points[1] = temp;
+                }
+                double t = (double)(temp_points[0].Y - temp_points[2].Y) / (temp_points[0].Y - temp_points[1].Y - temp_points[2].Y + temp_points[1].Y);
+                temp_points[3].X = temp_points[0].X + Convert.ToInt32((temp_points[2].X - temp_points[1].X) * t);
+                temp_points[3].Y = temp_points[0].Y + Convert.ToInt32((temp_points[2].Y - temp_points[1].Y) * t);
+                Draw(new Figure(temp_points));
+            }
+
+
+        }
+    }
+
+    public class Figure
+    {
+        private Point[] static_points;
+        private Point center;
+        public Figure()
+        {
+            static_points = new Point[4];
+            static_points[0].X = 200;
+            static_points[0].Y = 200;
+
+            static_points[1].X = 250;
+            static_points[1].Y = 300;
+
+            static_points[2].X = 450;
+            static_points[2].Y = 300;
+
+            static_points[3].X = 400;
+            static_points[3].Y = 200;
+
+            center = new Point(225,250);
+        }
+
+        public Figure(Point[] new_points)
+        {
+            static_points = new Point[4];
+            static_points[0].X = new_points[0].X;
+            static_points[0].Y = new_points[0].Y;
+
+            static_points[1].X = new_points[1].X;
+            static_points[1].Y = new_points[1].Y;
+
+            static_points[2].X = new_points[2].X;
+            static_points[2].Y = new_points[2].Y;
+
+            static_points[3].X = new_points[3].X;
+            static_points[3].Y = new_points[3].Y;
+            //объявление точки вращения вокруг собственной оси center = new Point(x_c, y_c);
+        }
+
+        public void setCenter(Point center)
+        {
+            this.center = center;
+        }
+
+        public void setPoints(Point[] new_points)
+        {
+
+        }
+
+        public Point[] getPoints()
+        {
+            return static_points;
+        }
+        // маштаб
+        public void CreateZoom()
+        {
+
+        }
+
+        // перенос 
+        public void CreateTranslate()
+        {
+
+        }
+
+        // поворот
+        public void CreateRotate()
+        {
+
+        }
+    }
+}

cbde43ef220227994c10122b095baab47c1cdeb4
Constantine Weismann <Constantine Weismann>
2016-10-20 00:45:46 +0700

    fixed: all methods

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,8 +37,13 @@
             this.label_x_cord = new System.Windows.Forms.Label();
             this.label_y_cord = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.barRotatingSpeed = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.barMoveSpeed = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.exeption_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.barRotatingSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barMoveSpeed)).BeginInit();
             this.SuspendLayout();
             // 
             // field
@@ -62,7 +67,7 @@
             // 
             // create_primitive
             // 
-            this.create_primitive.Location = new System.Drawing.Point(616, 12);
+            this.create_primitive.Location = new System.Drawing.Point(587, 12);
             this.create_primitive.Name = "create_primitive";
             this.create_primitive.Size = new System.Drawing.Size(104, 38);
             this.create_primitive.TabIndex = 2;
@@ -72,13 +77,14 @@
             // 
             // RenderTime
             // 
+            this.RenderTime.Enabled = true;
             this.RenderTime.Interval = 30;
             this.RenderTime.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(634, 53);
+            this.label1.Location = new System.Drawing.Point(740, 444);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(15, 13);
             this.label1.TabIndex = 3;
@@ -87,7 +93,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(634, 66);
+            this.label2.Location = new System.Drawing.Point(740, 457);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(15, 13);
             this.label2.TabIndex = 4;
@@ -96,7 +102,7 @@
             // label_x_cord
             // 
             this.label_x_cord.AutoSize = true;
-            this.label_x_cord.Location = new System.Drawing.Point(655, 53);
+            this.label_x_cord.Location = new System.Drawing.Point(761, 444);
             this.label_x_cord.Name = "label_x_cord";
             this.label_x_cord.Size = new System.Drawing.Size(36, 13);
             this.label_x_cord.TabIndex = 5;
@@ -105,7 +111,7 @@
             // label_y_cord
             // 
             this.label_y_cord.AutoSize = true;
-            this.label_y_cord.Location = new System.Drawing.Point(655, 66);
+            this.label_y_cord.Location = new System.Drawing.Point(761, 457);
             this.label_y_cord.Name = "label_y_cord";
             this.label_y_cord.Size = new System.Drawing.Size(36, 13);
             this.label_y_cord.TabIndex = 6;
@@ -120,25 +126,70 @@
             this.label3.TabIndex = 8;
             this.label3.Text = "Скорость\r\nповорота\r\nфигуры";
             // 
-            // trackBar1
+            // barRotatingSpeed
             // 
-            this.trackBar1.LargeChange = 1;
-            this.trackBar1.Location = new System.Drawing.Point(616, 153);
-            this.trackBar1.Maximum = 100;
-            this.trackBar1.Minimum = 1;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar1.Size = new System.Drawing.Size(45, 304);
-            this.trackBar1.TabIndex = 9;
-            this.trackBar1.Value = 1;
-            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.barRotatingSpeed.LargeChange = 1;
+            this.barRotatingSpeed.Location = new System.Drawing.Point(616, 153);
+            this.barRotatingSpeed.Maximum = 100;
+            this.barRotatingSpeed.Minimum = 1;
+            this.barRotatingSpeed.Name = "barRotatingSpeed";
+            this.barRotatingSpeed.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.barRotatingSpeed.Size = new System.Drawing.Size(45, 304);
+            this.barRotatingSpeed.TabIndex = 9;
+            this.barRotatingSpeed.Value = 56;
+            this.barRotatingSpeed.Scroll += new System.EventHandler(this.rotatingSpeed);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(674, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 39);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Скорость\r\nдвижения\r\nфигуры";
+            // 
+            // barMoveSpeed
+            // 
+            this.barMoveSpeed.LargeChange = 1;
+            this.barMoveSpeed.Location = new System.Drawing.Point(677, 153);
+            this.barMoveSpeed.Minimum = 1;
+            this.barMoveSpeed.Name = "barMoveSpeed";
+            this.barMoveSpeed.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.barMoveSpeed.Size = new System.Drawing.Size(45, 304);
+            this.barMoveSpeed.TabIndex = 11;
+            this.barMoveSpeed.Value = 6;
+            this.barMoveSpeed.Scroll += new System.EventHandler(this.moveSpeed);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(699, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 38);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Задать произвольно";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.enableInput);
+            // 
+            // exeption_label
+            // 
+            this.exeption_label.AutoSize = true;
+            this.exeption_label.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.exeption_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.exeption_label.Location = new System.Drawing.Point(12, 9);
+            this.exeption_label.Name = "exeption_label";
+            this.exeption_label.Size = new System.Drawing.Size(0, 13);
+            this.exeption_label.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(815, 510);
-            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.exeption_label);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.barMoveSpeed);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.barRotatingSpeed);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label_y_cord);
             this.Controls.Add(this.label_x_cord);
@@ -151,7 +202,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barRotatingSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barMoveSpeed)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,7 +219,11 @@
         private System.Windows.Forms.Label label_x_cord;
         private System.Windows.Forms.Label label_y_cord;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar barRotatingSpeed;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar barMoveSpeed;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label exeption_label;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 // для работы с библиотекой OpenGL 
@@ -20,162 +14,91 @@ namespace WindowsFormsApplication3
     public partial class Form1 : Form
     {
         Figure example;
-        private int count_clic = 0;
+        private short count_click = 0;
         private Point[] temp_points = new Point[4];
+        private bool flag_input = false;// триггер для произвольного ввода точек
+
         public Form1()
         {
             InitializeComponent();
             field.InitializeContexts();
+            field.MouseWheel += myMouseWheel;
         }
 
-        private void Draw(Figure figure)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+                temp_points[i] = new Point(-1, -1);
+            // инициализация Glut 
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_SINGLE);
+            // очистка окна 
+            Gl.glClearColor(0, 0, 0, 1);
+            // установка порта вывода в соответствии с размерами элемента anT 
+            Gl.glViewport(0, 0, field.Width, field.Height);
+            // настройка проекции 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            // теперь необходимо корректно настроить 2D ортогональную проекцию 
+            Glu.gluOrtho2D(0.0, (float)field.Width, 0.0, (float)field.Height);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+        }
+
+        private void Draw()
         {
             // очистка буфера цвета и буфера глубины 
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
             Gl.glClearColor(0, 0, 0, 1);
             // очищение текущей матрицы 
             Gl.glLoadIdentity();
-
             // установка черного цвета 
             Gl.glColor3f(0, 0, 0);
-
             // помещаем состояние матрицы в стек матриц 
             Gl.glPushMatrix();
-            Gl.glTranslated(example.getCenterX(), example.getCenterY(), 0.0f);
-            Gl.glRotated(example.getAngle(), 0, 0, 1);
-            Gl.glScalef((float)example.getScaleX(), (float)example.getScaleY(), 1);
-            Gl.glTranslated(example.getTranslateX(), example.getTranslateY(), 0.0f);
-            Gl.glTranslated(-example.getCenterX(), -example.getCenterY(), 0.0f);
-            // помещаем состояние матрицы в стек матриц 
-            Gl.glPushMatrix();
+            if (example != null)
+            {
+                Gl.glTranslated(example.getCenterX(), example.getCenterY(), 0.0f);
+                Gl.glRotated(example.getAngle(), 0, 0, 1);
+                Gl.glScalef((float)example.getScaleX(), (float)example.getScaleY(), 1);
+                Gl.glTranslated(example.getTranslateX(), example.getTranslateY(), 0.0f);
+                Gl.glTranslated(-example.getCenterX(), -example.getCenterY(), 0.0f);
+                Point[] points = example.getPoints();
+                Gl.glBegin(Gl.GL_POLYGON);
+                Gl.glColor3d(255, 0, 0);
 
-            Point[] points = figure.getPoints();
-            Gl.glBegin(Gl.GL_POLYGON);
-            Gl.glColor3d(255, 0, 0);
-            Gl.glVertex2f(points[0].X, points[0].Y);
-            Gl.glVertex2f(points[1].X, points[1].Y);
-            Gl.glVertex2f(points[2].X, points[2].Y);
-            Gl.glVertex2f(points[3].X, points[3].Y);
-            Gl.glEnd();
-            
-
+                Gl.glVertex2f(points[3].X, points[3].Y);
+                Gl.glVertex2f(points[0].X, points[0].Y);
+                Gl.glVertex2f(points[1].X, points[1].Y);
+                Gl.glVertex2f(points[2].X, points[2].Y);
+                Gl.glEnd();
+            }
+            if (flag_input)
+            {
+               Gl.glBegin(Gl.GL_POINTS);
+               Gl.glColor3d(255, 255, 255);
+                if (temp_points[0].X != -1)
+                    Gl.glVertex2d(temp_points[0].X, temp_points[0].Y);
+                if (temp_points[1].X != -1)
+                    Gl.glVertex2d(temp_points[1].X, temp_points[1].Y);
+                if (temp_points[2].X != -1)
+                    Gl.glVertex2d(temp_points[2].X, temp_points[2].Y);
+               Gl.glEnd();
+             }
+                
             // возвращаем состояние матрицы 
             Gl.glPopMatrix();
-
             // отрисовываем геометрию 
             Gl.glFlush();
-
             // обновляем состояние элемента 
             field.Invalidate();
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
-            // инициализация Glut 
-            Glut.glutInit();
-            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_SINGLE);
-            
-            // очистка окна 
-            Gl.glClearColor(0, 0, 0, 1);
-
-            // установка порта вывода в соответствии с размерами элемента anT 
-            Gl.glViewport(0, 0, field.Width, field.Height);
-
-
-            // настройка проекции 
-            Gl.glMatrixMode(Gl.GL_PROJECTION);
-            Gl.glLoadIdentity();
-
-            // теперь необходимо корректно настроить 2D ортогональную проекцию 
-            Glu.gluOrtho2D(0.0,  (float)field.Width, 0.0, (float)field.Height);
-            
-            Gl.glMatrixMode(Gl.GL_MODELVIEW);
-           
-            Gl.glLoadIdentity();
-
-            
-        }
-
         public void btn_create_primitive(object sender, EventArgs e)
         {
             example = new Figure();
-            RenderTime.Enabled = true;
-            Draw(example);
-        }
-        
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            
-            Draw(example);
-        }
-
-        private void field_KeyDown(object sender, KeyEventArgs e)
-        {
-
-            // Z и X отвечают за масштабирование 
-            if (e.KeyCode == Keys.Z)
-            {
-                // вызов функции, в которой мы реализуем масштабирование - передаем коэффициент масштабирования и выбранную ось в окне программы 
-                example.setScale(-0.05f, 0.00f);
-            }
-            if (e.KeyCode == Keys.X)
-            {
-                // вызов функции, в которой мы реализуем масштабирование - передаем коэффициент масштабирования и выбранную ось в окне программы 
-                example.setScale(0.05f, 0.00f);
-            }
-
-            if (e.KeyCode == Keys.C)
-            {
-                // вызов функции, в которой мы реализуем масштабирование - передаем коэффициент масштабирования и выбранную ось в окне программы 
-                example.setScale(0.0f, -0.03f);
-            }
-
-            if (e.KeyCode == Keys.V)
-            {
-                // вызов функции, в которой мы реализуем масштабирование - передаем коэффициент масштабирования и выбранную ось в окне программы 
-                example.setScale(0.0f, 0.03f);
-            }
-
-            // W и S отвечают за перенос 
-            if (e.KeyCode == Keys.W)
-            {
-                example.setTranslate(0, 1);
-                // вызов функции, в которой мы реализуем перенос - передаем значение перемещения и выбранную ось в окне программы 
-                
-            }
-            if (e.KeyCode == Keys.S)
-            {
-                example.setTranslate(0, -1);
-                // вызов функции, в которой мы реализуем перенос - передаем значение перемещения и выбранную ось в окне программы 
-
-
-            } // A и D отвечают за перемещение по Х 
-            if (e.KeyCode == Keys.A)
-            {
-                example.setTranslate(-1, 0);
-                // вызов функции, в которой мы реализуем поворот - передаем значение для поворота и выбранную ось 
-
-            }
-            if (e.KeyCode == Keys.D)
-            {
-                example.setTranslate(1, 0);
-            }
-            // Е и Q отвечают за поворот
-            if(e.KeyCode == Keys.E)
-            {
-                example.CreateRotate(1.0f);
-                label3.Text = "" + example.getAngle();
-            }
-
-            if (e.KeyCode == Keys.Q)
-            {
-                example.CreateRotate(-1.0f);
-                label3.Text = "" + example.getAngle();
-                label2.Text = "" + example.getSpeed();
-                example.speed = trackBar1.Value;
-            }
+            Draw();
         }
 
         private void field_Load(object sender, EventArgs e)
@@ -183,60 +106,157 @@ namespace WindowsFormsApplication3
 
         }
 
+
+
+        //************* функции обработчики **************
+        private void field_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (example != null)
+                switch (e.KeyCode)
+                {
+                    // W A S D отвечают за перемещение по x и y 
+                    case Keys.W:
+                        example.setTranslate(0, example.move_speed);
+                        break;
+                    case Keys.S:
+                        example.setTranslate(0, -example.move_speed);
+                        break;
+                    case Keys.A:
+                        example.setTranslate(-example.move_speed, 0);
+                        break;
+                    case Keys.D:
+                        example.setTranslate(example.move_speed, 0);
+                        break;
+                    // Z и X маштабирование по x
+                    case Keys.Z:
+                        example.setScale(-0.05f, 0.00f);
+                        break;
+                    case Keys.X:
+                        example.setScale(0.05f, 0.00f);
+                        break;
+                    //С и V маштабирование по y
+                    case Keys.C:
+                        example.setScale(0.0f, -0.03f);
+                        break;
+                    case Keys.V:
+                        example.setScale(0.0f, 0.03f);
+                        break;
+                    // Е и Q отвечают за поворот
+                    case Keys.E:
+                        example.CreateRotate(1.0f);
+                        break;
+                    case Keys.Q:
+                        example.CreateRotate(-1.0f);
+                        break;
+                }
+        }
+
         private void myMouse(object sender, MouseEventArgs e)
         {
-            //if (count_clic < 3)
-            //{
-            //    label_x_cord.Text = e.X + "";
-            //    label_y_cord.Text = field.Height - e.Y + "";
-            //    temp_points[count_clic].X = e.X;
-            //    temp_points[count_clic].Y = field.Height - e.Y;
-            //    count_clic++;
-            //}
-            //else
-            //{
-            //    count_clic = 0;
-            //    if ((temp_points[2].X - temp_points[0].X) * (temp_points[1].Y - temp_points[0].Y) - (temp_points[2].Y - temp_points[0].Y) * (temp_points[1].X - temp_points[0].X) > 0)
-            //    {
-            //        Point temp;
-            //        temp = temp_points[2];
-            //        temp_points[2] = temp_points[1];
-            //        temp_points[1] = temp;
-            //    }
-            //    double t = (double)(temp_points[0].Y - temp_points[2].Y) / (temp_points[0].Y - temp_points[1].Y - temp_points[2].Y + temp_points[1].Y);
-            //    temp_points[3].X = temp_points[0].X + Convert.ToInt32((temp_points[2].X - temp_points[1].X) * t);
-            //    temp_points[3].Y = temp_points[0].Y + Convert.ToInt32((temp_points[2].Y - temp_points[1].Y) * t);
-            //    Draw(new Figure(temp_points));
-            //}
             if (e.Button == MouseButtons.Left)
             {
-                example = new Figure();
-                RenderTime.Enabled = true;
-                Draw(example);
+                if (flag_input)
+                {
+
+                    if (count_click < 2)
+                    {
+                        temp_points[count_click].X = e.X;
+                        temp_points[count_click].Y = field.Height - e.Y;
+                        count_click++;
+                    }
+                    else
+                    {
+                        temp_points[count_click].X = e.X;
+                        temp_points[count_click].Y = field.Height - e.Y;
+                        count_click = 0;
+                        flag_input = false;
+                        if ((temp_points[2].X - temp_points[0].X) * (temp_points[1].Y - temp_points[0].Y) - (temp_points[2].Y - temp_points[0].Y) * (temp_points[1].X - temp_points[0].X) < 0)
+                        {
+                            Point p = temp_points[2];
+                            temp_points[2] = temp_points[1];
+                            temp_points[1] = p;
+                        }
+                        //warning t = NaN
+                        try
+                        {
+                            double t = (double)(temp_points[0].Y - temp_points[2].Y) / (temp_points[0].Y - temp_points[1].Y - temp_points[2].Y + temp_points[1].Y);
+                            temp_points[3].X = temp_points[0].X + Convert.ToInt32((temp_points[2].X - temp_points[1].X) * t);
+                            temp_points[3].Y = temp_points[0].Y + Convert.ToInt32((temp_points[2].Y - temp_points[1].Y) * t);
+                            //warning
+                            example = new Figure(temp_points);
+                            Draw();
+                            for (int i = 0; i < 3; i++)
+                                temp_points[i] = new Point(-1, -1);
+                        }
+                        catch (Exception)
+                        {
+                            exeption_label.Text = "Warning t = NaN, line 185 Введите точки заного";
+                            for (int i = 0; i < 3; i++)
+                                temp_points[i] = new Point(-1, -1);
+                        }
+
+                    }
+                }
             }
-            else {
-                if(e.Button == MouseButtons.Right)
+            if (e.Button == MouseButtons.Right)
+                if (example != null)
                     example.setCenter(new Point(e.X, e.Y));
-            }
-
         }
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
+        private void myMouseWheel(object sender, MouseEventArgs e)
         {
-            double x = (double)trackBar1.Value;
-            label1.Text = "" + x;
-            
+
+            if (e.Delta > 0)
+                if (example != null)
+                    example.setScale(-0.05f, -0.05f);
+            if (e.Delta < 0)
+                if (example != null)
+                    example.setScale(0.05f, 0.05f);
         }
+        //*************                     **************
+
+
+
+        //************* функции константы **************
+        private void rotatingSpeed(object sender, EventArgs e)
+        {
+            if (example != null)
+             example.rotating_speed = barRotatingSpeed.Value;
+            field.Select();// or .Focus(); It does not work
+        }
+
+        private void enableInput(object sender, EventArgs e)
+        {
+            exeption_label.Text = "";
+            flag_input = true;
+            field.Select();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void moveSpeed(object sender, EventArgs e)
+        {
+            if (example != null)
+                example.move_speed = barMoveSpeed.Value;
+            field.Focus();
+        }
+        
+        //*************               **************
     }
 
     public class Figure
     {
         private Point[] static_points;
-        private Point center;
+        private Point center = new Point(225, 250);
         private Point translate = new Point(0, 0);
-        public double speed;
+        public int move_speed;
+        public double rotating_speed;
         private double angle;
         private double[] scale = new double[2];
+
         public Figure()
         {
             static_points = new Point[4];
@@ -252,7 +272,8 @@ namespace WindowsFormsApplication3
             static_points[3].X = 400;
             static_points[3].Y = 200;
 
-            speed = 1.0f;
+            rotating_speed = 1.0f;
+            move_speed = 1;
 
             angle = 0;
 
@@ -278,17 +299,43 @@ namespace WindowsFormsApplication3
             static_points[3].X = new_points[3].X;
             static_points[3].Y = new_points[3].Y;
 
+            rotating_speed = 1.0f;
+            move_speed = 1;
             angle = 0;
-
+            scale[0] = 1;
+            scale[1] = 1;
             //объявление точки вращения вокруг собственной оси center = new Point(x_c, y_c);
         }
 
+        public void CreateRotate(double angle)
+        {
+            this.angle += angle * Math.PI / 120.0 * rotating_speed;
+        }
+
+        //************* set функции **************
         public void setTranslate(int x, int y)
         {
             translate.X += x;
             translate.Y += y;
         }
 
+        public void setScale(double x, double y)
+        {
+            scale[0] += x;
+            scale[1] += y;
+        }
+
+        public void setCenter(Point center)
+        {
+            this.center = center;
+        }
+
+        public void setPoints(Point[] new_points)
+        {
+
+        }
+
+        //************* get функции **************
         public double getTranslateX()
         {
             return translate.X;
@@ -303,43 +350,30 @@ namespace WindowsFormsApplication3
         {
             return this.center.X;
         }
+
         public double getCenterY()
         {
             return this.center.Y;
         }
-        public void setCenter(Point center)
+
+        public double getRotetingSpeed()
         {
-            this.center = center;
+            return rotating_speed;
         }
 
-        public void setSpeed(double x)
+        public double getMoveSpeed()
         {
-            this.speed = x;
-        }
-
-        public double getSpeed()
-        {
-            return speed;
-        }
-
-        public void setPoints(Point[] new_points)
-        {
-
+            return move_speed;
         }
 
         public double getAngle()
         {
             return angle;
         }
+
         public Point[] getPoints()
         {
             return static_points;
-        }
-        // маштаб
-        public void setScale(double x, double y)
-        {
-            scale[0] += x;
-            scale[1] += y;
         }
 
         public double getScaleX()
@@ -352,16 +386,5 @@ namespace WindowsFormsApplication3
             return scale[1];
         }
 
-        // перенос 
-        public void CreateTranslate()
-        {
-
-        }
-
-        // поворот
-        public void CreateRotate(double angle)
-        {
-            this.angle += angle * Math.PI / 120.0 * speed;
-        }
     }
 }
